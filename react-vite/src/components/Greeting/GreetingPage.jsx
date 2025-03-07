bc9c9939de380f104668794b29225d7a5963e095
Kolten <kolten.smith1@gmail.com>
2025-03-07 05:27:04 -0700

    minor correction

--- react-vite/src/components/Greeting/GreetingPage.jsx
+++ react-vite/src/components/Greeting/GreetingPage.jsx
@@ -2,16 +2,16 @@ import { NavLink } from "react-router-dom"
 import "./GreetingPage.css"
 function Greeting() {
     return (
-        <>
-            <div>
-                <h1>Hola amigos, ¡espero que estén bien!</h1>
+        <div className="Greeting-container">
+            <div className>
+                <h1 className="Greeting-title">Hola amigos, ¡espero que estén bien!</h1>
             </div>
-            <h1>Te damos la bienvenida a Rose</h1>
-            <div>
-                <h2>Aquí puedes ver la lista de todas las lecciones.</h2>
+            <h1 className="Greeting-title">Te damos la bienvenida a Rose</h1>
+            <div className="click-me">
+                <h2 className="Greeting-title">Aquí puedes ver la lista de todas las lecciones.</h2>
                 <NavLink to='/landing-page' className='nav-link'>Click Me!</NavLink>
             </div>
-        </>
+        </div>
     )
 }
 export default Greeting
